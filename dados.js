9b90c6cd521bf9a0f4a7d11399bc1c9ece1d8722
pjstudiodesign-cell <pjstudiodesign@gmail.com>
2026-04-29 10:53:50 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,22 +3,19 @@ const categoriasCardapio = [
         id: "cerveja",
         nome: "Cervejas",
         itens: [
-            // --- SUB-CATEGORIA: LITRÃO ---
-            { secao: true, nome: "--- LITRÃO ---" },
+            { tipo: "titulo", nome: "--- LITRÃO ---" },
             { id: 13, nome: "Skol Litrão", preco: 10.00, desc: "Cerveja Skol 1 Litro", img: "cerveja/cerfeja_skol_litrao_.jpg" },
             { id: 14, nome: "Brahma Litrão", preco: 10.00, desc: "Cerveja Brahma 1 Litro", img: "cerveja/brahma_litrao.jpg" },
             { id: 15, nome: "Antarctica Litrão", preco: 10.00, desc: "Cerveja Antarctica 1 Litro", img: "cerveja/antarctica_litrao.jpg" },
             { id: 48, nome: "Império Litrão", preco: 10.00, desc: "Cerveja Império 1 Litro", img: "cerveja/imperio_litrao.jpg" },
             { id: 50, nome: "Amstel Litrão", preco: 10.00, desc: "Cerveja Amstel 1 Litro", img: "cerveja/amstel_litrao.jpg" },
 
-            // --- SUB-CATEGORIA: 600ML ---
-            { secao: true, nome: "--- 600ML ---" },
+            { tipo: "titulo", nome: "--- 600ML ---" },
             { id: 49, nome: "Heineken 600ml", preco: 16.00, desc: "Cerveja Heineken 600ml", img: "cerveja/heineken_600ml.jpg" },
             { id: 51, nome: "Spaten 600ml", preco: 11.00, desc: "Cerveja Spaten 600ml", img: "cerveja/spaten_600ml.jpg" },
             { id: 52, nome: "Original 600ml", preco: 11.00, desc: "Cerveja Original 600ml", img: "cerveja/original_600ml.jpg" },
 
-            // --- SUB-CATEGORIA: LATÃO E LATAS ---
-            { secao: true, nome: "--- LATÃO E LATAS ---" },
+            { tipo: "titulo", nome: "--- LATÃO E LATAS ---" },
             { id: 43, nome: "Skol 550ml", preco: 6.50, desc: "Cerveja Skol Latão 550ml", img: "cerveja/skol_550.jpg" },
             { id: 44, nome: "Brahma 550ml", preco: 6.50, desc: "Cerveja Brahma Latão 550ml", img: "cerveja/brahma_550.jpg" },
             { id: 45, nome: "Antarctica 550ml", preco: 6.50, desc: "Cerveja Antarctica Latão 550ml", img: "cerveja/antarctica_550.jpg" },
